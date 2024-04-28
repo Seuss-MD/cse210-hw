fad447610079069b76bfa9fa63cfbd8bc0f7b041
Seuss-MD <myevil3000@gmail.com>
2024-04-27 18:10:45 -0600

    Variables Loops User Input Functions

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string fname;
+        string lname;
+
+        Console.WriteLine("What is your first name? ");
+        fname = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        lname = Console.ReadLine();
+
+        Console.WriteLine("Your name is "+ lname+ ", "+ fname+ ' '+ lname+'.');
     }
 }
